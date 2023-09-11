76ab0a6d449e52440e19b9a158a50f2aef5fbbcd
Jason <135300478+JC20002527@users.noreply.github.com>
2023-09-11 11:38:35 +0800

    Update player_bst.py

--- app/player_bst.py
+++ app/player_bst.py
@@ -1,8 +1,19 @@
+from app.player import Player
+
+
 class PlayerBST:
-    def __init__(self, root_value: int = None):
-        self._root = root_value
+    def __init__(self, root_value: Player = None):
+        self._root: Player | None = root_value
 
     @property
-    def root(self) -> int:
+    def root(self) -> Player:
         return self._root
-    
+
+    def insert(self, new_player: Player):
+        requested_name = new_player.name
+        if requested_name < self.root.name:
+            # insert left
+            self.insert(new_player)  # ?
+        elif requested_name > self.root.name:
+            # insert right
+            self.insert(new_player)  # ?
