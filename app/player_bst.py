80df93bf314275dcd5ff205eb2fe695789259e5d
Jason <135300478+JC20002527@users.noreply.github.com>
2023-09-21 10:51:30 +0800

    Implement recursive insert

--- app/player_bst.py
+++ app/player_bst.py
@@ -1,4 +1,5 @@
 from app.player import Player
+from app.player_bnode import PlayerBNode
 
 
 class PlayerBST:
@@ -9,11 +10,21 @@ class PlayerBST:
     def root(self) -> Player:
         return self._root
 
-    def insert(self, new_player: Player):
-        requested_name = new_player.name
-        if requested_name < self.root.name:
-            # insert left
-            self.insert(new_player)  # ?
-        elif requested_name > self.root.name:
-            # insert right
-            self.insert(new_player)  # ?
+    def insert(self, new_player: Player, current_node: PlayerBNode = None):
+        if current_node is None:
+            current_node = self.root
+        if new_player.name < current_node.value.name:
+            if current_node.left is None:
+                # insert left
+                current_node.left = PlayerBNode(new_player)
+                return
+            self.insert(new_player, current_node.left)
+        elif new_player.name > current_node.value.name:
+            if current_node.right is None:
+                # insert right
+                current_node.right = PlayerBNode(new_player)
+                return
+            self.insert(new_player, current_node.right)
+        elif new_player.name == current_node.value.name:
+            # overwrite player
+            current_node = PlayerBNode(new_player)
