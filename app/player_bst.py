8ab9944dfc2d61c83c0a738cf40ec3332c261d9e
Jason <135300478+JC20002527@users.noreply.github.com>
2023-09-21 11:37:55 +0800

    Step 6: Implement binary search

--- app/player_bst.py
+++ app/player_bst.py
@@ -15,16 +15,26 @@ class PlayerBST:
             current_node = self.root
         if new_player.name < current_node.value.name:
             if current_node.left is None:
-                # insert left
-                current_node.left = PlayerBNode(new_player)
+                current_node.left = PlayerBNode(new_player)  # insert left
                 return
             self.insert(new_player, current_node.left)
         elif new_player.name > current_node.value.name:
             if current_node.right is None:
-                # insert right
-                current_node.right = PlayerBNode(new_player)
+                current_node.right = PlayerBNode(new_player)  # insert right
                 return
             self.insert(new_player, current_node.right)
         elif new_player.name == current_node.value.name:
-            # overwrite player
-            current_node = PlayerBNode(new_player)
+            current_node = PlayerBNode(new_player)  # overwrite player
+
+    def search(self, name: str, current_node: PlayerBNode = None) -> PlayerBNode | None:
+        if current_node is None:
+            current_node = self.root
+        if name == current_node.value.name:
+            return current_node
+        if name < current_node.value.name and current_node.left is not None:
+            current_node = self.search(name, current_node.left)
+            return current_node
+        if name > current_node.value.name and current_node.right is not None:
+            current_node = self.search(name, current_node.right)
+            return current_node
+        return None
--- test/player_bst_test.py
+++ test/player_bst_test.py
@@ -7,19 +7,24 @@ from app.player_bst import PlayerBST
 class PlayerBSTTest(unittest.TestCase):
     def setUp(self) -> None:
         random_list = random.sample(range(100), 10)
-        names_list = ['john', 'jack', 'allan', 'jane', 'eliza', 'andrew', 'lewis', 'joe', 'beth', 'adam']
-        self.players = [Player(uid, names_list[index % len(names_list)])
+        self.names_list = ['john', 'jack', 'allan', 'jane', 'eliza', 'andrew', 'lewis', 'joe', 'beth', 'adam']
+        self.players = [Player(uid, self.names_list[index % len(self.names_list)])
                         for index, uid in enumerate(random_list)]
         self.tree = PlayerBST(self.players[0])
-
-    def test_inserting_players(self):
         self.tree.insert(self.players[1])
         self.tree.insert(self.players[2])
         self.tree.insert(self.players[3])
+
+    def test_inserting_players(self):
         self.assertFalse(self.tree.root.is_leaf)
         self.assertIs(self.tree.root.left.value, self.players[1])
         self.assertIs(self.tree.root.left.left.value, self.players[2])
         self.assertIs(self.tree.root.left.right.value, self.players[3])
+
+    def test_searching_tree(self):
+        self.assertEqual(self.tree.search(self.names_list[0]), self.tree.root)
+        self.assertEqual(self.tree.search(self.names_list[1]), self.tree.root.left)
+        self.assertEqual(self.tree.search(self.names_list[3]), self.tree.root.left.right)
 
 
 if __name__ == '__main__':
